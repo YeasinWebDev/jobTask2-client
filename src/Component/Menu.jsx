49289991e4af9ecce2db31803907d0c38cec7960
Yeasin <ar9485727@gmail.com>
2024-08-15 22:01:18 +0600

    add : Searching functionality

--- src/Component/Menu.jsx
+++ src/Component/Menu.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import useAxiosCommon from '../Hooks/useAxiosCommon'
+import { TiDeleteOutline } from "react-icons/ti";
 import Card from './Card'
 import Loading from './Loading'
 
 function Menu() {
     const [data, setData] = useState(null)
+    const [input, setInput] = useState('')
+    const [inputText, setinputText] = useState("")
     const [loading, setloading] = useState(false)
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const axiosCommon = useAxiosCommon()
 
-    const menuData = async (page) => {
+    const fetchData = async (page,query=null) => {
         setloading(true)
-        const result = await axiosCommon.post('/menu', null, {
+        const result = await axiosCommon.post(query? '/search':'/menu', query, {
             params: { page, limit: 9 },
         })
         setloading(false)
@@ -21,7 +24,7 @@ function Menu() {
     }
 
     useEffect(() => {
-        menuData(currentPage)
+        fetchData(currentPage)
     }, [currentPage])
 
 
@@ -31,13 +34,39 @@ function Menu() {
         }
     }
 
+    const handleSearch = async (e) => {
+        setCurrentPage(1);
+        await fetchData(1, { name: input });
+        setinputText(input);
+        setInput('');
+    }
+
     return (
         <div>
             <h1 className='font-semibold text-3xl py-10'>Our <span className='text-orange-600 font-bold'>Food</span></h1>
+            <div className='py-6'>
+                <div className='flex items-center justify-center gap-5'>
+                    <input type="text" className='border-2 border-orange-500 rounded-lg px-3 py-2 outline-none font-semibold text-lg' onChange={e => setInput(e.target.value)} value={input} placeholder='Search Food Name'/>
+                    <button className='text-white bg-orange-600 px-3 py-2 rounded-lg hover:bg-orange-800 ' onClick={handleSearch}>Search</button>
+                    {
+                        inputText
+                        &&
+                        <h1 className='flex items-center justify-center gap-2 bg-orange-600 text-white px-3 py-2 rounded-xl'>
+                            {inputText}
+                            <span className='cursor-pointer' onClick={() => (
+                                setinputText(''),
+                                fetchData(1)
+                            )}>
+                                <TiDeleteOutline size={29} color='white' />
+                            </span>
+                        </h1>
+                    }
+                </div>
+            </div>
             {
                 loading ? (
                     <div className='flex justify-center items-center h-screen'>
-                        <Loading/>
+                        <Loading />
                     </div>
                 ) : (
                     <div className='flex items-center justify-center gap-5 flex-wrap'>
