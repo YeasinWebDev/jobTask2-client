30ab5bbe6e40eda17182dd320fd7131790eb9049
Yeasin <ar9485727@gmail.com>
2024-08-15 20:27:45 +0600

    add: Footer

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='mt-10 border-t-2 border-orange-500'>
+            <footer className="footer text-base-content p-10">
+                <aside>
+                    <img src="/assets/logo.png" alt="" />
+                    <p>
+                        Providing reliable tech since 1992
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Nav from './Component/Nav'
+import Footer from './Component/Footer'
 
 function Root() {
     return (
         <div>
             <Nav/>
             <Outlet />
+            <Footer/>
         </div>
     )
 }
